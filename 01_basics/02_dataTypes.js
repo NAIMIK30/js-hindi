5bc53f5e60253f6783e88b465d05a42b2cbb086c
NAIMIK30 <shahnaimik30@gmail.com>
2025-06-24 06:38:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "naimik"
+let age = 20
+let isloggedIn = false
+let state = null
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isloggedIn);
+console.log(typeof state);
+
+
+
+
+
+
