b0f495529e2f1006468411cffb807f2aa9032193
NAIMIK30 <shahnaimik30@gmail.com>
2025-06-24 06:40:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 121213
+let accountEmail = "naimik@gmail.com"
+var accountPassword = "1234321"
+accountCity = "ghandhinagar"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
