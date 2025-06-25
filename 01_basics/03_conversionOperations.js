07b1cdf6a4fbae2159b6034c21e83cfd8d5c78c2
NAIMIK30 <shahnaimik30@gmail.com>
2025-06-25 09:29:04 +0000

    #continue

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -10,8 +10,23 @@ let isLoggedIn=1
 let booleanisloggedIn = Boolean(isLoggedIn)
 //console.log(booleanisloggedIn);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*************************************operation*************************************************** */
+
+// console.log(1 + 2 + "2");
+// console.log("1" + "1" + 2);
+// console.log("1" + "1");
+// console.log(4 + 5);
+// console.log((4 + 5) * 6 % 8) ;
+// console.log(+true);
+// console.log(+false);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 
